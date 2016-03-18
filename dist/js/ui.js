ab58e1a2d6bebe0ce67e3ad1c63f127eafe1218b
Gambuza <tapochamka@gmail.com>
2016-03-18 17:50:00 +0400

    главная страница

--- dist/js/ui.js
+++ dist/js/ui.js
@@ -1,20 +1,19 @@
+// Карусель
 $(document).ready(function() {
+ 
 $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
-    // nav:true,
-    autoplay:true,
-    autoplayTimeout:3000,
-    autoplayHoverPause:true,
+    nav:true,
     responsive:{
         0:{
             items:1
         },
         600:{
-            items:3
+            items:1
         },
         1000:{
-            items:5
+            items:1
         }
     }
 })
