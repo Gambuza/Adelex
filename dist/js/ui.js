685fe46df60f210a4c112fc9d9ee92930321471c
Gambuza <tapochamka@gmail.com>
2016-03-20 18:51:36 +0400

    внес

--- dist/js/ui.js
+++ dist/js/ui.js
@@ -18,3 +18,12 @@ $('.owl-carousel').owlCarousel({
     }
 })
 });
+
+$(document).ready(function() {
+$("a.scrolls").click(function () {
+var elementClick = $(this).attr("href")
+var destination = $(elementClick).offset().top;
+jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 800);
+return false;
+});
+});
