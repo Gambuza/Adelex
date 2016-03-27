4e4de62347db134a805f921618a33e443709e246
Gambuza <tapochamka@gmail.com>
2016-03-27 21:38:00 +0400

    коммит

--- dist/js/ui.js
+++ dist/js/ui.js
@@ -1,12 +1,17 @@
 // Слайдер
 $(document).ready(function(){
 	$(".owl-carousel").owlCarousel({
-		// nav:true,
+		  loop:true,
+		  margin:10,
+		 nav:true,
 		autoplay:true,
 		autoplayTimeout:5000,
 		responsive:{0:{items:1},600:{items:1},1e3:{items:1}}
 	})
 });
+// маркер на карте
+
+
         // Плавность скролла
 	$("a.scrolls").click(function(){
 		var t=$(this).attr("href"),
