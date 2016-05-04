e4dfaadb380accea48b5d225cfb1c964976b9d55
ilyapasuk <pasukilya@gmail.com>
2016-05-04 20:09:24 +0300

    slider & popup fix

--- dist/js/ui.js
+++ dist/js/ui.js
@@ -1,1 +1,71 @@
-$(document).ready(function(){$(".owl-carousel").owlCarousel({loop:!0,margin:10,nav:!0,responsive:{0:{items:1},600:{items:1},1e3:{items:1}}}),$("a.scrolls").click(function(){var t=$(this).attr("href"),o=$(t).offset().top;return jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop:o},800),!1}),$(".btn-primary").click(function(){$(this).parent().parent().find(".info").toggleClass("show"),$(this).parent().parent().find(".info").css("z-index",1),$(this).parent().find(".visible-trigger").toggleClass("show"),"Подробнее"==$(this).text()?$(this).text("Скрыть"):$(this).text("Подробнее")}),$("a#go").click(function(t){t.preventDefault(),$("#overlay").fadeIn(400,function(){$("#modal_form").css("display","block").animate({opacity:1,left:"0",top:"0",right:"0",bottom:"0"},200),$("body").css("overflow","hidden")})}),$("#modal_close, #overlay").click(function(){$("#modal_form").animate({opacity:0,top:"45%"},200,function(){$(this).css("display","none"),$("#overlay").fadeOut(400),$("body").css("overflow","auto")})})});
+// Карусель
+$(document).ready(function() {
+
+	$('.owl-carousel').owlCarousel({
+		loop: true,
+		margin: 10,
+		nav: true,
+		responsiveClass:true,
+		responsive: {
+			0: {
+				items: 1
+			}
+		}
+	})
+
+	$('.owl-carousel-modal').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+   	nav:false,
+   	dots: false,
+    responsive:{
+        0:{
+            items:1,
+        }
+    }
+})
+
+	$("a.scrolls").click(function() {
+		var elementClick = $(this).attr("href")
+		var destination = $(elementClick).offset().top;
+		jQuery("html:not(:animated),body:not(:animated)").animate({
+			scrollTop: destination
+		}, 800);
+		return false;
+	});
+
+	// Преимущества
+
+	$(".btn-primary").click(function() {
+		$(this).parent().parent().find('.info').toggleClass("show");
+		$(this).parent().parent().find('.info').css('z-index', 1);
+
+		$(this).parent().find('.visible-trigger').toggleClass("show");
+
+
+	  	// $( ".visible-trigger" ).show( "fast", function() {
+	   //  	// Animation complete.
+	  	// });
+
+	    if ($(this).text() == "Подробнее")
+	       $(this).text("Скрыть")
+	    else
+	       $(this).text("Подробнее");
+	});
+
+	// Модальное окно
+		$('a.go').click( function(event){ 
+			event.preventDefault(); 
+			$('#modal_form').addClass("open");
+			$('body').addClass("overflow");
+		});
+	// Закрытьß
+	$('#modal_close, #overlay').click( function(){ 
+		$('#modal_form').removeClass("open");
+		$('body').removeClass("overflow");
+	});
+
+});
+
+
--- js/ui.js
+++ js/ui.js
@@ -5,19 +5,26 @@ $(document).ready(function() {
 		loop: true,
 		margin: 10,
 		nav: true,
+		responsiveClass:true,
 		responsive: {
 			0: {
-				items: 1
-			},
-			600: {
-				items: 1
-			},
-			1000: {
 				items: 1
 			}
 		}
 	})
 
+	$('.owl-carousel-modal').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+   	nav:false,
+   	dots: false,
+    responsive:{
+        0:{
+            items:1,
+        }
+    }
+})
 
 	$("a.scrolls").click(function() {
 		var elementClick = $(this).attr("href")
@@ -48,26 +55,15 @@ $(document).ready(function() {
 	});
 
 	// Модальное окно
-		$('a#go').click( function(event){ 
-		event.preventDefault(); 
-		$('#overlay').fadeIn(400, 
-		 	function(){
-				$('#modal_form') 
-					.css('display', 'block') 
-					.animate({opacity: 1, left: '0', top: '0', right: '0', bottom: '0'}, 200); 
-				$('body').css('overflow', 'hidden');
+		$('a.go').click( function(event){ 
+			event.preventDefault(); 
+			$('#modal_form').addClass("open");
+			$('body').addClass("overflow");
 		});
-	});
 	// Закрытьß
 	$('#modal_close, #overlay').click( function(){ 
-		$('#modal_form')
-			.animate({opacity: 0, top: '45%'}, 200,
-				function(){ 
-					$(this).css('display', 'none'); 
-					$('#overlay').fadeOut(400);
-					$('body').css('overflow', 'auto');
-				}
-			);
+		$('#modal_form').removeClass("open");
+		$('body').removeClass("overflow");
 	});
 
 });
